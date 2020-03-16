6811c49deceaf8d05746f3915670d63700ba29f7
bomb697 <bomb697@users.noreply.github.com>
2020-03-16 17:37:12 +0100

    In MedianSelect mancava k-1 alla prima chiamata della funzione

--- src/MedianSelect.java
+++ src/MedianSelect.java
@@ -9,7 +9,7 @@ public class MedianSelect {
         System.out.print("Enter an integer: ");
         int k = input.nextInt();
 
-        System.out.print(array[MedianOfMedians(array, 0, array.length - 1, false, k++)] + "\n");
+        System.out.print(array[MedianOfMedians(array, 0, array.length - 1, false, k--)] + "\n");
     }
 
     static int[] getInputVector(String inputLine){
