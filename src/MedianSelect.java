c643a5bbfa3e3d56b1577f0a2e291b7a606707ba
Andrea Cantarutti <andreacantarutti@icloud.com>
2020-03-16 18:02:34 +0100

    Contratti su median select

--- src/MedianSelect.java
+++ src/MedianSelect.java
@@ -13,6 +13,11 @@ public class MedianSelect {
         System.out.print(array[MedianOfMedians(array, 0, array.length - 1, false, k)] + "\n");
     }
 
+    /**
+     * Splits the input line in all the different values
+     * @param inputLine required as a string of ints divided by one (or more) blank spaces
+     * @return an int array containing all the ints from the input
+     */
     static int[] getInputVector(String inputLine){
         String els[] = inputLine.split("\\s+");
         int length = els.length;
@@ -23,6 +28,16 @@ public class MedianSelect {
         return output;
     }
 
+
+    /**
+     * Computates the median of medians selection algorithm
+     * @param array the array with input values. REQUIRED not empty, at least one element
+     * @param l left index. REQUIRED 0<=l< array length
+     * @param r right index. REQUIRED 0<=r< array length
+     * @param p true only if I'm treating median of medians, othwerwise it's false
+     * @param k index for the algorithm, REQUIRED 0<=k< array length
+     * @return
+     */
     public static int MedianOfMedians(int[] array, int l, int r, boolean p, int k) {
         int i = l;
         int last5 = (r - l) - (r - l) % 5 + l;
@@ -63,6 +78,13 @@ public class MedianSelect {
         }
     }
 
+
+    /**
+     * Insertion sort algorithm. Sorts the array given as input
+     * @param array the array that will be sorted.  REQUIRED not empty
+     * @param l left index. REQUIRED 0<=l<array length
+     * @param r right index. REQUIRED 0<=r<array length
+     */
     public static void InsertionSort(int[] array, int l, int r) {
         int i = l + 1;
         while(i < r) {
@@ -75,6 +97,14 @@ public class MedianSelect {
         }
     }
 
+
+    /**
+     * Partitions the array according to the given index
+     * @param array the array that will be partitioned.  REQUIRED not empty
+     * @param l left index. REQUIRED 0<=l<array length
+     * @param r right index. REQUIRED 0<=r<array length
+     * @return the index for partitioning the array
+     */
     public static int partition(int[] array, int l, int r) {
         int pivot = array[l];
         int i = l + 1;
@@ -89,8 +119,15 @@ public class MedianSelect {
         return i;
     }
 
+
+    /**
+     * Swaps two elements of given index in an array
+     * @param array the array that will be partitioned.  REQUIRED not empty
+     * @param i index for the first element. REQUIRED 0<=i<array length
+     * @param j index for the second element. REQUIRED 0<=j<array length
+     */
     public static void swap(int[] array, int i, int j) {
-        int tmp = array[i];
+        int tmp = array[i];;;
         array[i] = array[j];
         array[j] = tmp;
     }
