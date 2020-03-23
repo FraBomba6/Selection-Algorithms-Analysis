c2c7423e6ac3392d337da69176ba7cb9652887d9
Andrea Cantarutti <andreacantarutti@icloud.com>
2020-03-23 14:59:07 +0100

    For computating resolution

---
+++ src/Resolution.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+import java.util.Vector;
+
+public class Resolution {
+    public static void main(String[] args){
+        long start, end, res;
+        Vector<Long> resolutions = new Vector<>();
+        for(int i=1; i<=100; i++) {
+            start = System.nanoTime();
+            do {
+                end = System.nanoTime();
+            } while (start == end);
+            res = end - start;
+            resolutions.add(res);
+        }
+
+        Collections.sort(resolutions);
+        Long median = resolutions.get(50);
+        System.out.println(median);
+    }
+
+
+}
