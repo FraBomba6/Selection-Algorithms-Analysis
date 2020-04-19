377fca740fbd926d8ad7356a1dc300ad20da5bc7
bomb697 <50354770+bomb697@users.noreply.github.com>
2020-04-19 11:48:18 +0200

    Modificate delle classi, aggioirnati i tempi e creato file per il changelog

--- src/Excel.java
+++ src/Excel.java
@@ -2,20 +2,27 @@ import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Excel {
     public static void main(String[] args) throws FileNotFoundException {
         try {
+            String templateName = "Time_Template.xlsx";
             String fileName = "Time.xlsx";
+
+            Files.copy(Paths.get(templateName), Paths.get(fileName));
             //Initializing a new excel file and sheet in which data will be registered
             FileInputStream inputStream = new FileInputStream(new File(fileName));
+
             Workbook workbook = WorkbookFactory.create(inputStream);
 
             //Fills the excel sheet
             for (int i = 1; i < 50; i++) {
                 double x = 1.58 * i;
                 Sheet sheet = workbook.cloneSheet(0);
-                workbook.setSheetName(i, String.valueOf((int)(Math.pow(1.22, x/1.8)*10)));
+                workbook.setSheetName(i, String.valueOf((int)(Math.pow(1.3, x/1.6)*10)));
             }
             //terminates after filling the sheet with 100 time execution's observations for each algorithm
 
--- src/Time.java
+++ src/Time.java
@@ -20,7 +20,7 @@ public class Time {
             //For each iteration generates a targetsize for the array based on the exponential function
             for(int iter = 0; iter < 50; iter++){
                 x = 1.58 * (double)iter;
-                targetSize = (int)(Math.pow(1.25, x/1.8)*10);
+                targetSize = (int)(Math.pow(1.3, x/1.6)*10);
 
                 //builds the random filled array based on the target size and sets 4 different values for k
                 int[] kArray = {4, (targetSize-1)/2, (int)(Math.log(targetSize)), (targetSize - 5)};
@@ -28,9 +28,9 @@ public class Time {
                 //Compute the execution time 50 times for every algorithm choosing a different k every time.
                 for (int i = 0; i < 4; i++) {
                     int[] input = RandomTest.randomInput(targetSize);
-                    System.out.println("Sheet " + iter + "    k " + i);
                     int k = kArray[i];
                     Sheet sheet = workbook.getSheetAt(iter);
+                    System.out.println("Sheet " + iter + "    Array length: " + sheet.getSheetName() + "    k: " + kArray[i]);
                     for (int row_index = 4; row_index < 54; row_index++) {
                         //New line
                         Row row = sheet.getRow(row_index);
